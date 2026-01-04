1119538125efa895e5d7d9409f29f94885d50596
Voredevor <devordan@gmail.com>
2026-01-04 03:11:17 -0800

    ytt

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from 'react-router-dom'
-import logo from '../assests/logo/logo.png'
+import logo from '../assets/logo/logo.png'
 import { useState } from 'react'
 
 const navLinks = [
